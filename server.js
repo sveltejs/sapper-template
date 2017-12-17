28b3db6534a7eca14235423288b92c69fcad72a0
Rich Harris <richard.a.harris@gmail.com>
2017-12-17 15:07:21 -0500

    update sapper, tidy up

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const static = require('serve-static');
 
 const { PORT = 3000 } = process.env;
 
+// this allows us to do e.g. `fetch('/api/blog')` on the server
 const fetch = require('node-fetch');
 global.fetch = (url, opts) => {
 	if (url[0] === '/') url = `http://localhost:${PORT}${url}`;
@@ -16,9 +17,7 @@ app.use(compression({ threshold: 0 }));
 
 app.use(static('assets'));
 
-app.use(sapper({
-	selector: '#sapper'
-}));
+app.use(sapper());
 
 app.listen(PORT, () => {
 	console.log(`listening on port ${PORT}`);
