0119b07e6b6da77b7538f4d7f3315d670a0886a8
Rich Harris <richard.a.harris@gmail.com>
2018-03-17 10:55:02 -0400

    use polka again

--- app/server.js
+++ app/server.js
@@ -1,21 +1,20 @@
 import { resolve } from 'url';
-import express from 'express';
+import polka from 'polka';
 import compression from 'compression';
 import sapper from 'sapper';
 import serve from 'serve-static';
 import fetch from 'node-fetch';
-import { basepath, routes } from './manifest/server.js';
+import { routes } from './manifest/server.js';
 
 const { PORT } = process.env;
 
 // this allows us to do e.g. `fetch('/api/blog-posts')` on the server
 global.fetch = (url, opts) => {
-	url = resolve(`http://localhost:${PORT}${basepath}/`, url);
+	url = resolve(`http://localhost:${PORT}/`, url);
 	return fetch(url, opts);
 };
 
-express()
+polka()
 	.use(compression({ threshold: 0 }))
-	.use(basepath, serve('assets'))
-	.use(sapper({ routes }))
+	.use(serve('assets'), sapper({ routes }))
 	.listen(PORT);
