8887791e725858b3f6fc9c8d69b07890cc146492
Rich Harris <richard.a.harris@gmail.com>
2018-03-18 22:38:14 -0400

    use fetch, tidy up

--- app/server.js
+++ app/server.js
@@ -1,20 +1,10 @@
-import { resolve } from 'url';
 import polka from 'polka';
 import compression from 'compression';
 import sapper from 'sapper';
 import serve from 'serve-static';
-import fetch from 'node-fetch';
 import { routes } from './manifest/server.js';
 
-const { PORT } = process.env;
-
-// this allows us to do e.g. `fetch('/api/blog-posts')` on the server
-global.fetch = (url, opts) => {
-	url = resolve(`http://localhost:${PORT}/`, url);
-	return fetch(url, opts);
-};
-
-polka()
+polka() // You can also use Express
 	.use(compression({ threshold: 0 }))
 	.use(serve('assets'), sapper({ routes }))
-	.listen(PORT);
+	.listen(process.env.PORT);
