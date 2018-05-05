98813dd8fde5bf6f1351aca2efff70a035ceccc3
Rich Harris <richard.a.harris@gmail.com>
2018-05-04 22:36:17 -0400

    experimental new structure

--- app/server.js
+++ app/server.js
@@ -3,11 +3,15 @@ import compression from 'compression';
 import sapper from 'sapper';
 import serve from 'serve-static';
 import { routes } from './manifest/server.js';
+import App from './App.html';
 
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		serve('assets'),
-		sapper({ routes })
+		sapper({
+			routes,
+			App
+		})
 	)
 	.listen(process.env.PORT);
