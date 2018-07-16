64837ca8a6b343fc3e47ac8843ba140937b7418d
Rich Harris <richard.a.harris@gmail.com>
2018-07-16 12:30:38 -0400

    updates

--- app/client.js
+++ app/client.js
@@ -1,9 +1,7 @@
 import { init } from 'sapper/runtime.js';
 import { routes } from './manifest/client.js';
-import App from './App.html';
 
 init({
 	target: document.querySelector('#sapper'),
-	routes,
-	App
+	routes
 });
--- app/server.js
+++ app/server.js
@@ -3,13 +3,12 @@ import polka from 'polka';
 import sapper from 'sapper';
 import compression from 'compression';
 import { routes } from './manifest/server.js';
-import App from './App.html';
 
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('assets'),
-		sapper({ routes, App })
+		sapper({ routes })
 	)
 	.listen(process.env.PORT)
 	.catch(err => {
