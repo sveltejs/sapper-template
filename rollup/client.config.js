10d30e4ea458d09176dd25595d03f128b984c049
Rich Harris <richard.a.harris@gmail.com>
2018-08-30 20:54:56 -0400

    aspirational approach to differential bundling

--- rollup/client.config.js
+++ rollup/client.config.js
@@ -2,11 +2,13 @@ import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
+import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
+const legacy = process.env.SAPPER_LEGACY_BUILD;
 
 export default {
 	input: config.client.input(),
@@ -21,6 +23,15 @@ export default {
 		svelte({
 			dev,
 			hydratable: true
+		}),
+
+		legacy && babel({
+			extensions: ['.js', '.html'],
+			include: [
+				'app/**',
+				'routes/**'
+			],
+			runtimeHelpers: true
 		}),
 
 		!dev && terser({
