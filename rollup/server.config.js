55f1e172f05af11a84aeca4fa26e188d4a65c522
Rich Harris <richard.a.harris@gmail.com>
2018-09-02 15:04:47 -0400

    use Rollup

---
+++ rollup/server.config.js
@@ -0,0 +1,37 @@
+import resolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import svelte from 'rollup-plugin-svelte';
+import config from 'sapper/config/rollup.js';
+import pkg from '../package.json';
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+
+export default {
+	input: config.server.input(),
+	output: config.server.output(),
+	plugins: [
+		svelte({
+			generate: 'ssr',
+			dev
+		}),
+		resolve(),
+		replace({
+			'process.browser': true,
+			'process.env.NODE_ENV': JSON.stringify(mode)
+		}),
+		commonjs()
+	],
+	external: Object.keys(pkg.dependencies).concat(
+		require('module').builtinModules
+	),
+
+	// temporary, pending Rollup 1.0
+	experimentalCodeSplitting: true,
+
+	// temporary, pending https://github.com/rollup/rollup/pull/2432
+	watch: {
+		chokidar: { disableGlobbing: true }
+	}
+};
---
+++ rollup/service-worker.config.js
@@ -0,0 +1,27 @@
+import resolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+
+export default {
+	input: config.serviceworker.input(),
+	output: config.serviceworker.output(),
+	plugins: [
+		resolve(),
+		replace({
+			'process.browser': true,
+			'process.env.NODE_ENV': JSON.stringify(mode)
+		}),
+		commonjs(),
+		!dev && terser()
+	],
+
+	// temporary, pending https://github.com/rollup/rollup/pull/2432
+	watch: {
+		chokidar: { disableGlobbing: true }
+	}
+};
