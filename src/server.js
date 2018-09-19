6fc5c000792dd235e3b9dc48cf400f413c650d93
Rich Harris <richard.a.harris@gmail.com>
2018-09-19 14:49:46 -0400

    move src files

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const dev = NODE_ENV === 'development';
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
-		sirv('assets', { dev }),
+		sirv('static', { dev }),
 		sapper({ manifest })
 	)
 	.listen(PORT, err => {
