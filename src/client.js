390c5e4df65d3966aa73f423fba2178879af86c3
Robert Malko <robmalko@gmail.com>
2018-12-30 11:00:59 -0800

    More tabs to spaces for src directory

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import * as sapper from '../__sapper__/client.js';
 
 sapper.start({
-	target: document.querySelector('#sapper')
+  target: document.querySelector('#sapper')
 });
--- src/routes/about.html
+++ src/routes/about.html
@@ -1,5 +1,5 @@
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
