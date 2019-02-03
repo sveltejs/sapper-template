fb4a6eff95928e331407e575cb7935634029692f
Richard Harris <richard.a.harris@gmail.com>
2019-02-03 09:23:53 -0500

    separate branch creation from pushing for sake of local dev

--- _template/build.sh
+++ _template/build.sh
@@ -6,28 +6,7 @@ cd "$(dirname $0)"/..
 echo "$SSH_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-# make sure we're on master, and delete the rollup-v3 and webpack-v3 branches
-git symbolic-ref HEAD refs/heads/master
-git reset --hard
-git branch -D rollup-v3 webpack-v3
-
-# create the rollup-v3 branch off the current master
-git checkout -b rollup-v3
-node _template/build-pkg.js rollup
-git rm -r --cached .travis.yml _template package_template.json webpack.config.js
-git add package.json
-git commit -m 'Sapper template for Rollup'
-git symbolic-ref HEAD refs/heads/master
-git reset --hard
-
-# create the webpack-v3 branch off the current master
-git checkout -b webpack-v3
-node _template/build-pkg.js webpack
-git rm -r --cached .travis.yml _template package_template.json rollup.config.js
-git add package.json
-git commit -m 'Sapper template for webpack'
-git symbolic-ref HEAD refs/heads/master
-git reset --hard
+./create-branches.sh
 
 # force push rollup-v3 and webpack branches
 git push git@github.com:sveltejs/sapper-template.git rollup-v3 webpack-v3 -f
