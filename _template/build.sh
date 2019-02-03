b888f6bf2434af6fd7fec1ae258a91173474cd85
Richard Harris <richard.a.harris@gmail.com>
2019-02-03 09:29:55 -0500

    spotted the flaw in my plan - it keeps kicking me back to master

--- _template/build.sh
+++ _template/build.sh
@@ -6,28 +6,12 @@ cd "$(dirname $0)"/..
 echo "$SSH_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-# make sure we're on master, and delete the rollup and webpack branches
-git symbolic-ref HEAD refs/heads/master
-git reset --hard
-git branch -D rollup webpack
+# branch names
+DEFAULT=master
+ROLLUP=rollup
+WEBPACK=webpack
 
-# create the rollup branch off the current master
-git checkout -b rollup
-node _template/build-pkg.js rollup
-git rm -r --cached .travis.yml _template package_template.json webpack.config.js
-git add package.json
-git commit -m 'Sapper template for Rollup'
-git symbolic-ref HEAD refs/heads/master
-git reset --hard
-
-# create the webpack branch off the current master
-git checkout -b webpack
-node _template/build-pkg.js webpack
-git rm -r --cached .travis.yml _template package_template.json rollup.config.js
-git add package.json
-git commit -m 'Sapper template for webpack'
-git symbolic-ref HEAD refs/heads/master
-git reset --hard
+./create-branches.sh $DEFAULT $ROLLUP $WEBPACK
 
 # force push rollup and webpack branches
-git push git@github.com:sveltejs/sapper-template.git rollup webpack -f
+git push git@github.com:sveltejs/sapper-template.git $ROLLUP $WEBPACK -f
