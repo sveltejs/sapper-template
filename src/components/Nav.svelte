ef073b0c038eadf859d5a977180259faa0fa2bec
Richard Harris <richard.a.harris@gmail.com>
2019-05-02 09:28:07 -0400

    update assets

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,7 +4,7 @@
 
 <style>
 	nav {
-		border-bottom: 1px solid rgba(170,30,30,0.1);
+		border-bottom: 1px solid rgba(255,62,0,0.1);
 		font-weight: 300;
 		padding: 0 1em;
 	}
@@ -36,7 +36,7 @@
 		content: '';
 		width: calc(100% - 1em);
 		height: 2px;
-		background-color: rgb(170,30,30);
+		background-color: rgb(255,62,0);
 		display: block;
 		bottom: -1px;
 	}
