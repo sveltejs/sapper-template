07fc656bba0f1b999afd38334573bc3a9c0436ab
Conduitry <git@chor.date>
2019-06-14 11:36:30 -0400

    rollup: also suppress CIRCULAR_DEPENDENCY warnings on Windows

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && warning.message.includes('/@sapper/')) || onwarn(warning);
+const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
 
 export default {
 	client: {
