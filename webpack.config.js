08f35d7ba488a16783dfed2c3835b56e8ca06436
InstanceOfMichael <InstanceOfMichael@users.noreply.github.com>
2019-08-11 10:39:49 -0400

    Update webpack.config.js
    Add `~` alias. This will allow users to use in their svelte apps the `~` alias like this:
    
    ```
      import AddUser from '~/components/AddUser.svelte';
    ```
    instead of:
    ```
      import AddUser from './AddUser.svelte';
    ```
    or
    ```
      import AddUser from '../../../AddUser.svelte';
    ```
    or
    ```
      import AddUser from '../components/AddUser.svelte';
    ```
    
    this would make it easier to leave nuxt: https://nuxtjs.org/guide/directory-structure/#aliases

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ const pkg = require('./package.json');
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
-const alias = { svelte: path.resolve('node_modules', 'svelte') };
+const alias = {
+	svelte: path.resolve('node_modules', 'svelte'),
+	'~': `${__dirname}/src`
+};
 const extensions = ['.mjs', '.js', '.json', '.svelte', '.html'];
 const mainFields = ['svelte', 'module', 'browser', 'main'];
 
