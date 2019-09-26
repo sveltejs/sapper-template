14cbe7caae48a2e276c0283183135cdcefd320d5
Conduitry <git@chor.date>
2019-09-26 04:49:31 -0400

    rework building templates

--- _template/build.sh
+++ _template/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-cd "$(dirname $0)"
+cd "$(dirname "$0")"
 
 if [ "$CI" ]; then
-	# write out SSH key
-	[ "$SSH_KEY" ] || exit 1
-	echo "$SSH_KEY" > ~/.ssh/id_rsa
-	chmod 600 ~/.ssh/id_rsa
+	(umask 0077; echo "$SSH_KEY" > ~/ssh_key; echo "$SSH_KEY_ROLLUP" > ~/ssh_key_rollup; echo "$SSH_KEY_WEBPACK" > ~/ssh_key_webpack)
+	git config user.email 'noreply@svelte.dev'
+	git config user.name '[bot]'
 fi
 
 # branch names
-DEFAULT=master
 ROLLUP=rollup
 WEBPACK=webpack
 
-./create-branches.sh $DEFAULT $ROLLUP $WEBPACK
+./create-branches.sh $ROLLUP $WEBPACK
 
-# force push rollup and webpack branches
-git push git@github.com:sveltejs/sapper-template.git $ROLLUP $WEBPACK -f
+# force push rollup and webpack branches and repos
+GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key' git push git@github.com:sveltejs/sapper-template.git $ROLLUP $WEBPACK -f
+GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key_rollup' git push git@github.com:sveltejs/sapper-template-rollup.git $ROLLUP:master -f
+GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=accept-new -i ~/ssh_key_webpack' git push git@github.com:sveltejs/sapper-template-webpack.git $WEBPACK:master -f
