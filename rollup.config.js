880963dd7e6947cf1b4f7f5339bc79ed8106318b
Matt Pilott <matt@creativelittledots.co.uk>
2019-11-02 12:16:32 +0000

    Switched to @rollup/plugin-replace

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
+import replace from '@rollup/plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
 import babel from 'rollup-plugin-babel';
