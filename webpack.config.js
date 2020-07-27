c62da55adbfccce1b60920f151406f2ed12cbede
Carlos Roso <ce.roso398@gmail.com>
2020-07-27 13:07:38 -0500

    use 'mode' consistently in webpack config
    This will prevent confusion like [this one](https://stackoverflow.com/q/61988779/3468917) where users want to change `mode` to 'development' by changing the top-level `const mode = process.env.NODE_ENV;` only to realize it has no effect in the `server` and `serviceworker` configurations.

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
 				}
 			]
 		},
-		mode: process.env.NODE_ENV,
+		mode,
 		performance: {
 			hints: false // it doesn't matter if server.js is large
 		}
@@ -72,6 +72,6 @@ module.exports = {
 	serviceworker: {
 		entry: config.serviceworker.entry(),
 		output: config.serviceworker.output(),
-		mode: process.env.NODE_ENV
+		mode
 	}
 };
