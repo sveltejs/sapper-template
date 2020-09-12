2d59e3762ac880c174a5f9bb6423e0c58eea57ce
mhatvan <markus_hatvan@aon.at>
2020-09-13 00:59:15 +0200

    - change formatting style to conform to project - add types programmatically to service-worker.ts - add compilerOptions lib for service worker types
    Signed-off-by: mhatvan <markus_hatvan@aon.at>

--- scripts/setupTypeScriptRollup.js
+++ scripts/setupTypeScriptRollup.js
@@ -1,60 +1,64 @@
 // @ts-check
-const fs = require("fs")
-const path = require("path")
-const { argv } = require("process")
+const fs = require('fs');
+const path = require('path');
+const { argv } = require('process');
 
-const projectRoot = argv[2] || path.join(__dirname, "..")
+const projectRoot = argv[2] || path.join(__dirname, '..');
 
-console.log("Adding typescript with rollup")
+console.log('Adding typescript with rollup');
 
 // Add deps to pkg.json
-let pkgJSONPath = path.join(projectRoot, "package.json")
-const packageJSON = JSON.parse(fs.readFileSync(pkgJSONPath, "utf8"))
+const pkgJSONPath = path.join(projectRoot, 'package.json');
+const packageJSON = JSON.parse(fs.readFileSync(pkgJSONPath, 'utf8'));
 packageJSON.devDependencies = Object.assign(packageJSON.devDependencies, {
-	"svelte-check": "^1.0.0",
-	"svelte-preprocess": "^4.0.0",
-	"@rollup/plugin-typescript": "^6.0.0",
-	"typescript": "^3.9.3",
-	"tslib": "^2.0.0",
-	"@tsconfig/svelte": "^1.0.0",
-	"@types/compression": "^1.7.0",
-	"@types/node": "^14.0.27",
-	"@types/polka": "^0.5.1"
-})
+	'svelte-check': '^1.0.0',
+	'svelte-preprocess': '^4.0.0',
+	'@rollup/plugin-typescript': '^6.0.0',
+	'typescript': '^3.9.3',
+	'tslib': '^2.0.0',
+	'@tsconfig/svelte': '^1.0.0',
+	'@types/compression': '^1.7.0',
+	'@types/node': '^14.0.27',
+	'@types/polka': '^0.5.1'
+});
 
 // Add script for checking
 packageJSON.scripts = Object.assign(packageJSON.scripts, {
-	validate: "svelte-check --ignore src/node_modules/@sapper"
-})
+	validate: 'svelte-check --ignore src/node_modules/@sapper'
+});
 
 // Write the package JSON
-fs.writeFileSync(pkgJSONPath, JSON.stringify(packageJSON, null, "  "))
+fs.writeFileSync(pkgJSONPath, JSON.stringify(packageJSON, null, '  '));
 
-scanFolderAndReplace(path.join(projectRoot, "src"))
+scanFolderAndReplace(path.join(projectRoot, 'src'));
 
 // replace js script tags to ts
 // replace js filenames to ts
 function scanFolderAndReplace(dir) {
-	const elements = fs.readdirSync(dir, { withFileTypes: true })
+	const elements = fs.readdirSync(dir, { withFileTypes: true });
 
 	for (let i = 0; i < elements.length; i++) {
-		if (elements[i].isDirectory())
-			scanFolderAndReplace(path.join(dir, elements[i].name))
-		else if (elements[i].name.match(/.svelte/)) {
-			let svelteFile = fs.readFileSync(path.join(dir, elements[i].name), "utf8")
-			svelteFile = svelteFile.replace("<script", '<script lang="ts"')
-			fs.writeFileSync(path.join(dir, elements[i].name), svelteFile)
-		} else if (elements[i].name.match(/^((?!json).)*js$/))
+		if (elements[i].isDirectory()) {
+			scanFolderAndReplace(path.join(dir, elements[i].name));
+		} else if (elements[i].name.match(/.svelte/)) {
+			let svelteFile = fs.readFileSync(
+				path.join(dir, elements[i].name),
+				'utf8'
+			);
+			svelteFile = svelteFile.replace('<script', '<script lang="ts"');
+			fs.writeFileSync(path.join(dir, elements[i].name), svelteFile);
+		} else if (elements[i].name.match(/^((?!json).)*js$/)) {
 			fs.renameSync(
 				path.join(dir, elements[i].name),
-				path.join(dir, elements[i].name).replace(".js", ".ts")
-			)
+				path.join(dir, elements[i].name).replace('.js', '.ts')
+			);
+		}
 	}
 }
 
 // Edit rollup config
-const rollupConfigPath = path.join(projectRoot, "rollup.config.js")
-let rollupConfig = fs.readFileSync(rollupConfigPath, "utf8")
+const rollupConfigPath = path.join(projectRoot, 'rollup.config.js');
+let rollupConfig = fs.readFileSync(rollupConfigPath, 'utf8');
 
 // Edit imports
 rollupConfig = rollupConfig.replace(
@@ -62,76 +66,138 @@ rollupConfig = rollupConfig.replace(
 	`'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';`
-)
+);
 
 // Edit inputs
 rollupConfig = rollupConfig.replace(
 	`input: config.client.input(),`,
 	`input: config.client.input().replace(/\.js$/, '.ts'),`
-)
+);
 rollupConfig = rollupConfig.replace(
 	`input: config.server.input(),`,
 	`input: { server: config.server.input().server.replace(/\.js$/, ".ts") },`
-)
+);
 rollupConfig = rollupConfig.replace(
 	`input: config.serviceworker.input(),`,
 	`input: config.serviceworker.input().replace(/\.js$/, '.ts'),`
-)
+);
 
 // Add preprocess to the svelte config, this is tricky because there's no easy signifier.
 // Instead we look for 'hydratable: true,'
 rollupConfig = rollupConfig.replace(
-	new RegExp("hydratable: true,", "g"),
-	"hydratable: true,\n\t\t\t\tpreprocess: sveltePreprocess(),"
-)
+	new RegExp('hydratable: true,', 'g'),
+	'hydratable: true,\n\t\t\t\tpreprocess: sveltePreprocess(),'
+);
+
+// Edit service worker
+const serviceWorkerConfigPath = path.join(
+	projectRoot,
+	'src',
+	'service-worker.ts'
+);
+
+let serviceWorkerConfig = fs.readFileSync(serviceWorkerConfigPath, 'utf8');
+
+serviceWorkerConfig = serviceWorkerConfig.replace(
+	`'@sapper/service-worker';`,
+	`'@sapper/service-worker';\n
+type Clientable = { clients: Clients };`
+);
+
+serviceWorkerConfig = serviceWorkerConfig.replace(
+	`shell.concat(files);`,
+	`(shell as string[]).concat(files as string[]);`
+);
+
+serviceWorkerConfig = serviceWorkerConfig.replace(
+	`const cached = new Set(to_cache);`,
+	`const cached = new Set(to_cache);\n
+const claimClient = (input: Clientable) => {
+	input.clients.claim();
+};
+
+const skipWaiting = (input: ServiceWorkerGlobalScope) => {
+	input.skipWaiting();
+};`
+);
+
+serviceWorkerConfig = serviceWorkerConfig.replace(
+	`'install', event =>`,
+	`'install', <EventType extends ExtendableEvent>(event: EventType) =>`
+);
+
+serviceWorkerConfig = serviceWorkerConfig.replace(
+	`self.skipWaiting();`,
+	`skipWaiting((self as any) as ServiceWorkerGlobalScope);`
+);
+
+serviceWorkerConfig = serviceWorkerConfig.replace(
+	`'activate', event =>`,
+	`'activate', <EventType extends ExtendableEvent>(event: EventType) =>`
+);
+
+serviceWorkerConfig = serviceWorkerConfig.replace(
+	`self.clients.claim();`,
+	`claimClient((self as any) as { clients: Clients });`
+);
+
+serviceWorkerConfig = serviceWorkerConfig.replace(
+	`'fetch', event =>`,
+	`'fetch', <EventType extends FetchEvent>(event: EventType) =>`
+);
+
+fs.writeFileSync(serviceWorkerConfigPath, serviceWorkerConfig);
 
 // Add TypeScript
 rollupConfig = rollupConfig.replace(
 	/commonjs\(\),?/g,
-	"commonjs(),\n\t\t\ttypescript({ sourceMap: dev }),"
-)
+	'commonjs(),\n\t\t\ttypescript({ sourceMap: dev }),'
+);
 
 // Save rollup config
-fs.writeFileSync(rollupConfigPath, rollupConfig)
+fs.writeFileSync(rollupConfigPath, rollupConfig);
 
 // Add TSConfig
 const tsconfig = `{
 	"extends": "@tsconfig/svelte/tsconfig.json",
+	"compilerOptions": {
+		"lib": ["DOM", "ES2017", "WebWorker"]
+	},
 	"include": ["src/**/*", "src/node_modules/**/*"],
 	"exclude": ["node_modules/*", "__sapper__/*", "static/*"]
-}`
-const tsconfigPath = path.join(projectRoot, "tsconfig.json")
-fs.writeFileSync(tsconfigPath, tsconfig)
+}`;
+const tsconfigPath = path.join(projectRoot, 'tsconfig.json');
+fs.writeFileSync(tsconfigPath, tsconfig);
 
 // Delete this script, but not during testing
 if (!argv[2]) {
 	// Remove the script
-	fs.unlinkSync(path.join(__filename))
+	fs.unlinkSync(path.join(__filename));
 
 	// Check for Mac's DS_store file, and if it's the only one left remove it
-	const remainingFiles = fs.readdirSync(path.join(__dirname))
-	if (remainingFiles.length === 1 && remainingFiles[0] === ".DS_store") {
-		fs.unlinkSync(path.join(__dirname, ".DS_store"))
+	const remainingFiles = fs.readdirSync(path.join(__dirname));
+	if (remainingFiles.length === 1 && remainingFiles[0] === '.DS_store') {
+		fs.unlinkSync(path.join(__dirname, '.DS_store'));
 	}
 
 	// Check if the scripts folder is empty
 	if (fs.readdirSync(path.join(__dirname)).length === 0) {
 		// Remove the scripts folder
-		fs.rmdirSync(path.join(__dirname))
+		fs.rmdirSync(path.join(__dirname));
 	}
 }
 
 // Adds the extension recommendation
-fs.mkdirSync(path.join(projectRoot, ".vscode"))
+fs.mkdirSync(path.join(projectRoot, '.vscode'));
 fs.writeFileSync(
-	path.join(projectRoot, ".vscode", "extensions.json"),
+	path.join(projectRoot, '.vscode', 'extensions.json'),
 	`{"recommendations": ["svelte.svelte-vscode"]}`
-)
+);
 
-console.log("Converted to TypeScript.")
+console.log('Converted to TypeScript.');
 
-if (fs.existsSync(path.join(projectRoot, "node_modules"))) {
+if (fs.existsSync(path.join(projectRoot, 'node_modules'))) {
 	console.log(
-		"\nYou will need to re-run your dependency manager to get started."
-	)
+		'\nYou will need to re-run your dependency manager to get started.'
+	);
 }
