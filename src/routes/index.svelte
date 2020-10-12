bc410e44441562143990ee510bc9d7e1710e143f
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-10-12 16:01:11 -0700

    double quotes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -43,7 +43,7 @@
 <h1>Great success!</h1>
 
 <figure>
-	<img alt='Success Kid' src='{successkid}'>
+	<img alt="Success Kid" src="{successkid}">
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
