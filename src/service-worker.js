aed9446f8dc1684ed9df2fbf374dd9f8ab839d98
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2020-10-13 08:03:43 +0200

    formatting

--- src/service-worker.js
+++ src/service-worker.js
@@ -56,7 +56,7 @@ self.addEventListener('fetch', event => {
 	const isHttp = url.protocol.startsWith('http');
 	const isDevServerRequest = url.hostname === self.location.hostname && url.port !== self.location.port;
 	const isStaticAsset = url.host === self.location.host && cached.has(url.pathname);
-	const skipBecauseUncached = event.request.cache === "only-if-cached" && !isStaticAsset;
+	const skipBecauseUncached = event.request.cache === 'only-if-cached' && !isStaticAsset;
 
 	if (isHttp && !isDevServerRequest && !skipBecauseUncached) {
 		event.respondWith(
