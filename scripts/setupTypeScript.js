dfac515bc0b7cda7cb623f13fee25b3d627e05c5
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-12-16 08:20:20 -0800

    Fix addition of preprocess to Rollup config in TypeScript setup script

--- scripts/setupTypeScript.js
+++ scripts/setupTypeScript.js
@@ -161,9 +161,8 @@ import typescript from '@rollup/plugin-typescript';
 			/input: config.serviceworker.input\(\)(?!\.replace)/,
 			`input: config.serviceworker.input().replace(/\\.js$/, '.ts')`
 		],
-		// Add preprocess to the svelte config, this is tricky because there's no easy signifier.
-		// Instead we look for 'hydratable: true,'
-		[/hydratable: true(?!,\n\s*preprocess)/g, 'hydratable: true,\n\t\t\t\tpreprocess: sveltePreprocess()'],
+		// Add preprocess
+		[/compilerOptions/g, 'preprocess: sveltePreprocess(),\n\t\t\t\tcompilerOptions'],
 		// Add TypeScript
 		[/commonjs\(\)(?!,\n\s*typescript)/g, 'commonjs(),\n\t\t\ttypescript({ sourceMap: dev })']
 	]);
