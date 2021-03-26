4ddc022fe064d34be9c76653cccb839d3633f9f0
_uchanlee <uchanlee.dev@gmail.com>
2021-03-27 00:16:59 +0900

    fix: show ts error when importing images (#291)

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -10,30 +10,30 @@
  */
 declare module "*.gif" {
 	const value: string;
-	export = value;
+	export default value;
 }
 
 declare module "*.jpg" {
 	const value: string;
-	export = value;
+	export default value;
 }
 
 declare module "*.jpeg" {
 	const value: string;
-	export = value;
+	export default value;
 }
 
 declare module "*.png" {
 	const value: string;
-	export = value;
+	export default value;
 }
 
 declare module "*.svg" {
 	const value: string;
-	export = value;
+	export default value;
 }
 
 declare module "*.webp" {
 	const value: string;
-	export = value;
+	export default value;
 }
